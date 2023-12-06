8221affbd99e1a5de9ad840d4d0e57975e9e3797
KEIMYga <keimykgonzalesamutari@gmail.com>
2023-12-06 18:07:09 -0400

    add: Styles

--- src/main/java/uap/fit/cetic/controller/LaboratorioController.java
+++ src/main/java/uap/fit/cetic/controller/LaboratorioController.java
@@ -1,10 +1,14 @@
 package uap.fit.cetic.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import uap.fit.cetic.model.entity.Laboratorio;
+import uap.fit.cetic.model.enums.Categoria;
 import uap.fit.cetic.model.service.ILaboratorioService;
 
 @Controller
@@ -15,6 +19,34 @@ public class LaboratorioController {
 
   @GetMapping("/lista")
   public String listar(Model model) {
+    model.addAttribute("listaLAboratorio", laboratorioService.listarTodos());
     return "laboratorio/lista";
   }
+   //Este metodo de get es para enviar datos a la vista
+  @GetMapping("/form-registrar")
+  public String formRegistro(Model model) {
+    model.addAttribute("listaCategoria", Categoria.values());
+    model.addAttribute("laboratorio", new Laboratorio());
+    return "vista-laboratorio/laboratorio-form";
+  }
+
+  //Este metodo de get es para enviar datos a la vista
+  @GetMapping("/form-modificar/{idLaboratorio}")
+  public String formModificar(Model model, @PathVariable Long idLaboratorio) {
+    model.addAttribute("listaCategoria", Categoria.values());
+    model.addAttribute("laboratorio", laboratorioService.buscarPorId(idLaboratorio));
+    return "vista-laboratorio/laboratorio-form";
+  }
+    @PostMapping("/registrar")
+  public String registrar(@ModelAttribute Laboratorio laboratorio) {
+    laboratorioService.guardar(laboratorio);
+    return "redirect://lista";
+  }
+  @ResponseBody
+  @DeleteMapping("/{idLaboratorio}")
+  public ResponseEntity<String> eliminar(@PathVariable Long idLaboratorio) {
+    laboratorioService.eliminarPorId(idLaboratorio);  
+    return ResponseEntity.ok("Laboratorio eliminado correctamente, id: " + idLaboratorio); 
+  }
+  
 }
